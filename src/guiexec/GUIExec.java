93e5952bd413ba5c1a502fe7a37b2d093621bfba
SENOO Ken <mslamsh20131029@outlook.jp>
2017-08-21 00:07:12 +0900

    Implement UI

--- src/guiexec/GUIExec.java
+++ src/guiexec/GUIExec.java
@@ -20,7 +20,8 @@ public class GUIExec extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        
+//        VBox root = (VBox) FXMLLoader.load(getClass().getResource("main.fxml"));
+
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
--- src/guiexec/mainController.java
+++ src/guiexec/mainController.java
@@ -5,12 +5,19 @@
  */
 package guiexec;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -21,10 +28,48 @@ public class mainController implements Initializable {
     @FXML
     private Label label;
     
+    @FXML private TextField path;
+    
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+    }
+    
+    public void run(){
+      try {
+        // Process process = new ProcessBuilder("sh", "/usr/local/bin/shellScript.sh").start();
+
+        Process process = new ProcessBuilder("pwd").start();
+
+        String text;
+        InputStream is = process.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+        BufferedReader reader = new BufferedReader(isr);
+        StringBuilder builder = new StringBuilder();
+        int c;
+        while ((c = reader.read()) != -1) {
+          builder.append((char)c);
+        }
+        // 実行結果を格納
+        text = builder.toString();
+        int ret = process.waitFor();
+
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setContentText(text);
+        
+        alert.showAndWait();
+//        System.out.println(text);
+//        System.out.println(ret);
+
+      } catch (IOException | InterruptedException e) {
+        e.printStackTrace();
+      }    
+    }
+    
+    public void fileSelect(){
+        
+    
     }
     
     @Override
